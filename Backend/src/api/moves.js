f864fb595f57c59346118544a121dcbb11722671
Rybov <jankowalczyk38@gmail.com>
2024-12-31 12:22:26 +0100

    update db import

--- Backend/src/api/moves.js
+++ Backend/src/api/moves.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import db from '../db/db.js';
+import { db } from '../db/db.js';
 import Queries from "../db/queries.js";
 
 const router = express.Router();
--- Backend/src/api/movies.js
+++ Backend/src/api/movies.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import db from '../db/db.js';
+import { db } from '../db/db.js';
 
 export const moviesRoutes = express.Router();
 
